7f6a1c5d403fe4f96280b8df73fbac42d691cdb8
Johannes Windelen <mail@johanneswindelen.com>
2021-02-17 16:50:18 -0800

    fix paths

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ set -e
 
 function install {
 	# dpkg-query -l zsh 2>&1 |  awk '/no package found /{c++}END{print a}' > /dev/null && return
-	sudo apt-get install -y $1
+	sudo apt-get install -y $1 > /dev/null 2>&1
 }
+
 
 echo "Installing git..."
 install "git"
@@ -13,26 +14,27 @@ install "git"
 DDIR=~/.dotfiles
 
 echo "Cloning repository https://github.com/lookingfortrees/dotfiles..."
-git clone https://github.com/lookingfortrees/dotfiles $DDIR &> /dev/null
+git clone --quiet https://github.com/lookingfortrees/dotfiles $DDIR > /dev/null
 
 echo "Installing tmux..."
 install "tmux"
 mkdir -p ~/.config/tmux
-ln $DDIR/config/tmux ~/.config/tmux
-ln -s ~/.config/.tmux.conf ~/.tmux.conf
-ln -s ~/.config/.tmux.conf.local ~/.tmux.conf.local
+ln --force $DDIR/files/tmux.conf ~/.tmux.conf
+ln --force $DDIR/files/tmux.conf.local ~/.tmux.conf.local
 
 echo "Installing vim..."
 install "vim"
 mkdir -p ~/.config/vim
-ln $DDIR/config/vim ~/.config/vim
+# ln $DDIR/files/vim ~/.config/vim
 
 echo "Installing zsh..."
 install "zsh zsh-autosuggestions"
-mkdir -p ~/.config/zsh
-ln $DDIR/config/zsh ~/.config/zsh
-ln -s ~/.config/zsh/zshrc ~/.zshrc
+ln --force -s $DDIR/files/zsh ~/.config/
+ln --force $DDIR/files/zshrc ~/.zshrc
 
 echo "Installing ssh..."
 mkdir -p ~/.ssh
-ln -s $DDIR/config/sshconfig ~/.ssh/config
+# ln -s $DDIR/files/sshconfig ~/.ssh/config
+
+echo "Setting shell to zsh..."
+chsh -s $(which zsh) $(whoami)
