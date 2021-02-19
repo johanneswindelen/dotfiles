19351d8aee877b04ee2a923ec8d6dc9af2b90f9d
Johannes Windelen <mail@johanneswindelen.com>
2021-02-19 07:39:26 -0800

    add linux/darwin switches for aliases and zsh-autocompletion locations

--- files/zsh/alias.zsh
+++ files/zsh/alias.zsh
@@ -1,5 +1,13 @@
-alias ls="ls --color=auto"
-alias ll="ls --color=auto -la"
+case $OSTYPE in
+  darwin*)
+    alias ll="ls -laG"
+    alias ls="ls -G"
+  ;;
+  linux*)
+    alias ll="ls -la --color=auto"
+    alias ls="ls --color=auto"
+  ;;
+esac
 alias grep="grep --color=auto"
 alias gp="git symbolic-ref --short HEAD 2> /dev/null | xargs -L1 git push origin"
 alias ssh="TERM=xterm-256color ssh"
