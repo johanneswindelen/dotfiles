d66c6a65698bce01b5ebabc720e9f835b8430bc4
Johannes Windelen <mail@johanneswindelen.com>
2021-02-23 10:32:56 -0800

    add support for rerunning script

--- install.sh
+++ install.sh
@@ -1,45 +1,69 @@
 #! /bin/bash
+# this script install_pkgs my config files and base programs on both debian and macos systems
 
-set -e
+# -e stops script execution if a command fails
+# -o pipefail propagates errors through pipes, i.e. `doesnt_exist | doesnt_exist | return 0` fails
+set -e -o pipefail
 
-function install {
+# directory for the REPODIRsitory
+REPODIR=~/.dotfiles
+
+# update package index
+function update_pkgs {
     case $OSTYPE in
         linux*)
-            sudo apt-get install -y $1 > /dev/null
+            sudo apt-get update > /dev/null
 	    ;;
 	darwin*)
-            brew install $1 > /dev/null
+            brew update > /dev/null
 	    ;;
     esac
 }
 
+# install a given package
+function install_pkg {
+    case $OSTYPE in
+        linux*)
+            sudo apt-get install_pkg -y $1 > /dev/null
+	    ;;
+	darwin*)
+            HOMEBREW_NO_AUTO_UPDATE=1 brew install_pkg -q $1 > /dev/null
+	    ;;
+    esac
+}
+
+echo "Updating package list"
+update_pkgs
 
 echo "Installing git..."
-install "git"
+install_pkg "git"
 
-DDIR=~/.dotfiles
+if [ -d $REPODIR ]; then
+    echo "Removing previous repository at $REPODIR"
+    rm -rf $REPODIR
+fi
 
-echo "Cloning repository https://github.com/lookingfortrees/dotfiles..."
-git clone --quiet https://github.com/lookingfortrees/dotfiles $DDIR > /dev/null
+echo "Cloning repository https://github.com/lookingfortrees/dotfiles to $REPODIR"
+git clone --quiet https://github.com/lookingfortrees/dotfiles $REPODIR > /dev/null
 
 echo "Installing tmux..."
-install "tmux"
-ln -f $DDIR/files/tmux.conf ~/.tmux.conf
-ln -f $DDIR/files/tmux.conf.local ~/.tmux.conf.local
+install_pkg "tmux"
+ln -f $REPODIR/files/tmux.conf ~/.tmux.conf
+ln -f $REPODIR/files/tmux.conf.local ~/.tmux.conf.local
 
 echo "Installing vim..."
-install "vim"
-ln $DDIR/files/vimrc ~/.vimrc
+install_pkg "vim"
+ln -f $REPODIR/files/vimrc ~/.vimrc
 
 echo "Installing zsh..."
-install "zsh zsh-autosuggestions"
+install_pkg "zsh zsh-autosuggestions"
 mkdir -p ~/.config
-ln -f -s $DDIR/files/zsh ~/.config/
-ln -f $DDIR/files/zshrc ~/.zshrc
+ln -sf $REPODIR/files/zsh ~/.config
+ln -f $REPODIR/files/zshrc ~/.zshrc
 
-echo "Installing ssh..."
+echo "Installinging ssh..."
 mkdir -p ~/.ssh
-# ln -s $DDIR/files/sshconfig ~/.ssh/config
+# ln -s $REPODIR/files/sshconfig ~/.ssh/config
 
 echo "Setting shell to zsh..."
 sudo chsh -s $(which zsh) $(whoami)
